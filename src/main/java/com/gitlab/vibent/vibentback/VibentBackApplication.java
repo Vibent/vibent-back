fc4a9849f103e11f1efaf8a07a6dd192c4c2aa87
Conor Ryan <conor.fr.ryan@gmail.com>
2018-02-09 12:36:49 +0100

    initial commit

---
+++ src/main/java/com/gitlab/vibent/vibentback/VibentBackApplication.java
@@ -0,0 +1,12 @@
+package com.gitlab.vibent.vibentback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibentBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibentBackApplication.class, args);
+	}
+}
