9c5d7500a30ae0e644c65e08fc886230c8f2193a
JoubertM <theojoubertmoureaud@gmail.com>
2018-02-11 19:54:38 +0100

    Added foreign keys on sql FUCK ME THAT WAS PURE HAPPINESS

--- src/main/resources/vibent.sql
+++ src/main/resources/vibent.sql
@@ -26,8 +26,8 @@ CREATE TABLE group_t
 CREATE TABLE event_participation
 (
     id INT NOT NULL AUTO_INCREMENT,
-    user_ref CHAR(36) REFERENCES user(ref),
-    group_ref CHAR(36) REFERENCES group_t(ref),
+    user_ref CHAR(36),
+    group_ref CHAR(36),
     answer ENUM('Yes', 'No', 'Maybe', 'Unanswered'),
     is_visible BOOLEAN,
     PRIMARY KEY(id)
@@ -37,7 +37,7 @@ CREATE TABLE event
 (
     id INT NOT NULL AUTO_INCREMENT,
     ref CHAR(36) NOT NULL,
-    group_ref CHAR(36) NOT NULL REFERENCES group_t(ref),
+    group_ref CHAR(36) NOT NULL,
     title VARCHAR(255),
     description VARCHAR(500),
     start_date DATETIME,
@@ -59,7 +59,7 @@ CREATE TABLE group_membership
 CREATE TABLE group_invite_link
 (
     id INT NOT NULL AUTO_INCREMENT,
-    group_ref CHAR(36) NOT NULL REFERENCES group_t(ref),
+    group_ref CHAR(36) NOT NULL,
     hash VARCHAR(255),
     expires DATETIME,
     PRIMARY KEY(id)
@@ -68,7 +68,7 @@ CREATE TABLE group_invite_link
 CREATE TABLE bubble_ownership
 (
     id INT NOT NULL AUTO_INCREMENT,
-    event_ref CHAR(36) NOT NULL REFERENCES event(id),
+    event_ref CHAR(36) NOT NULL,
     bubble_type ENUM('TravelBubble', 'LocationBubble', 'AlimentationBubble', 'SurveyBubble','CheckBoxBubble', 'PlanningBubble', 'FreeBubble'),
     bubble_id INT,
     creator_ref CHAR(36),
@@ -84,8 +84,8 @@ CREATE TABLE travel_bubble
 CREATE TABLE travel_proposal
 (
     id INT NOT NULL AUTO_INCREMENT,
-    bubble_id INT REFERENCES travel_bubble(id),
-    driver_ref CHAR(36) REFERENCES user(ref),
+    bubble_id INT,
+    driver_ref CHAR(36),
     capacity INT,
     pass_by_cities VARCHAR(500),
     PRIMARY KEY(id)
@@ -94,8 +94,8 @@ CREATE TABLE travel_proposal
 CREATE TABLE travel_request
 (
     id INT NOT NULL AUTO_INCREMENT,
-    creator_ref CHAR(36) REFERENCES user(ref),
-    bubble_id INT REFERENCES travel_bubble(id),
+    creator_ref CHAR(36),
+    bubble_id INT,
     capacity INT,
     attached_to_proposal BOOLEAN,
     PRIMARY KEY(id)
@@ -104,8 +104,8 @@ CREATE TABLE travel_request
 CREATE TABLE attached_request
 (
     id INT NOT NULL AUTO_INCREMENT,
-    proposal_id INT REFERENCES travel_proposal(id),
-	  request_id INT REFERENCES travel_request(id),
+    proposal_id INT,
+	  request_id INT,
     PRIMARY KEY(id)
 );
 
@@ -114,7 +114,6 @@ CREATE TABLE location_bubble
     id INT NOT NULL AUTO_INCREMENT,
     coord VARCHAR(255),
     PRIMARY KEY(id)
-
 );
 
 CREATE TABLE alimentation_bubble
@@ -126,7 +125,7 @@ CREATE TABLE alimentation_bubble
 CREATE TABLE alimentation_entry
 (
     id INT NOT NULL AUTO_INCREMENT,
-    bubble_id INT REFERENCES alimentation_bubble(id),
+    bubble_id INT,
     name VARCHAR(64),
     total_requested INT,
     total_current INT,
@@ -137,7 +136,7 @@ CREATE TABLE alimentation_entry
 CREATE TABLE alimentation_bringing
 (
     id INT NOT NULL AUTO_INCREMENT,
-    entry_id INT REFERENCES alimentation_entry(id),
+    entry_id INT,
     user_ref CHAR(36),
     quantity INT,
     PRIMARY KEY(id)
@@ -153,7 +152,7 @@ CREATE TABLE survey_bubble
 CREATE TABLE survey_response
 (
     id INT NOT NULL AUTO_INCREMENT,
-    survey_id INT REFERENCES survey_bubble(id),
+    survey_id INT,
     content VARCHAR(500),
     PRIMARY KEY(id)
 );
@@ -161,8 +160,8 @@ CREATE TABLE survey_response
 CREATE TABLE users_survey_responses
 (
     id INT NOT NULL AUTO_INCREMENT,
-    user_ref CHAR(36) REFERENCES user(ref),
-    surveyresponse_id INT REFERENCES survey_response(id),
+    user_ref CHAR(36),
+    surveyresponse_id INT,
     PRIMARY KEY(id)
 );
 
@@ -176,7 +175,7 @@ CREATE TABLE checkbox_bubble
 CREATE TABLE checkbox_response
 (
    id INT NOT NULL AUTO_INCREMENT,
-   bubble_id INT REFERENCES checkbox_bubble(id),
+   bubble_id INT,
    content VARCHAR(500),
    PRIMARY KEY(id)
 );
@@ -213,3 +212,123 @@ CREATE TABLE free_bubble
     content VARCHAR(1000),
     PRIMARY KEY(id)
 );
+
+ALTER TABLE user
+ADD UNIQUE KEY user_ref (ref);
+
+ALTER TABLE group_t
+ADD UNIQUE KEY group_ref (ref);
+
+ALTER TABLE checkbox_bubble
+ADD UNIQUE KEY checkbox_bubble_id (id);
+
+ALTER TABLE checkbox_response
+ADD UNIQUE KEY checkbox_response_id(id);
+
+ALTER TABLE event
+ADD UNIQUE KEY event_id (id),
+ADD UNIQUE KEY event_ref (ref);
+
+ALTER TABLE travel_bubble
+ADD UNIQUE KEY travel_bubble_id (id);
+
+ALTER TABLE travel_proposal
+ADD UNIQUE KEY travel_proposal_id (id);
+
+ALTER TABLE travel_request
+ADD UNIQUE KEY travel_request_id (id);
+
+ALTER TABLE alimentation_bubble
+ADD UNIQUE KEY alimentation_bubble_id (id);
+
+ALTER TABLE alimentation_entry
+ADD UNIQUE KEY alimentation_entry_id (id);
+
+ALTER TABLE survey_bubble
+ADD UNIQUE KEY survey_bubble_id(id);
+
+ALTER TABLE survey_response
+ADD UNIQUE KEY survey_response_id(id);
+
+ALTER TABLE planning_bubble
+ADD UNIQUE KEY planning_bubble_id(id);
+
+
+
+ALTER TABLE event_participation
+ADD CONSTRAINT event_participation_user__fk
+FOREIGN KEY (user_ref) REFERENCES user(ref),
+ADD CONSTRAINT event_participation_group__fk
+FOREIGN KEY (group_ref) REFERENCES group_t(ref);
+
+ALTER TABLE event
+ADD CONSTRAINT event_group__fk
+FOREIGN KEY (group_ref)REFERENCES group_t(ref);
+
+ALTER TABLE group_membership
+ADD CONSTRAINT group_membership_user__fk
+FOREIGN KEY (user_ref) REFERENCES user(ref),
+ADD CONSTRAINT group_membership_group__fk
+FOREIGN KEY (group_ref) REFERENCES group_t(ref);
+
+ALTER TABLE group_invite_link
+ADD CONSTRAINT group_invite_link_group__fk
+FOREIGN KEY (group_ref)REFERENCES group_t(ref);
+
+ALTER TABLE bubble_ownership
+ADD CONSTRAINT bubble_ownership_event__fk
+FOREIGN KEY (event_ref)REFERENCES event(ref),
+ADD CONSTRAINT bubble_ownership_user__fk
+FOREIGN KEY (creator_ref)REFERENCES user(ref);
+
+ALTER TABLE travel_proposal
+ADD CONSTRAINT travel_proposal_bubble__fk
+FOREIGN KEY (bubble_id) REFERENCES travel_bubble(id),
+ADD CONSTRAINT travel_proposal_driver__fk
+FOREIGN KEY (driver_ref) REFERENCES user(ref);
+
+ALTER TABLE travel_request
+ADD CONSTRAINT travel_request_creator__fk
+FOREIGN KEY (creator_ref) REFERENCES user(ref),
+ADD CONSTRAINT travel_request_bubble__fk
+FOREIGN KEY (bubble_id) REFERENCES travel_bubble(id);
+
+ALTER TABLE attached_request
+ADD CONSTRAINT attached_request_proposal__fk
+FOREIGN KEY (proposal_id) REFERENCES travel_proposal(id),
+ADD CONSTRAINT attached_request_request__fk
+FOREIGN KEY (request_id) REFERENCES travel_request(id);
+
+ALTER TABLE alimentation_entry
+ADD CONSTRAINT alimentation_entry_bubble__fk
+FOREIGN KEY (bubble_id)REFERENCES alimentation_bubble(id);
+
+ALTER TABLE alimentation_bringing
+ADD CONSTRAINT alimentation_bringing_alimentry__fk
+FOREIGN KEY (entry_id)REFERENCES alimentation_entry(id);
+
+ALTER TABLE survey_response
+ADD CONSTRAINT survey_response_bubble__fk
+FOREIGN KEY (survey_id)REFERENCES survey_bubble(id);
+
+ALTER TABLE users_survey_responses
+ADD CONSTRAINT users_survey_responses_user__fk
+FOREIGN KEY (user_ref)REFERENCES user(ref),
+ADD CONSTRAINT users_survey_responses_bubble__fk
+FOREIGN KEY (surveyresponse_id)REFERENCES survey_response(id);
+
+ALTER TABLE checkbox_response
+ADD CONSTRAINT checkbox_response_bubble__fk
+FOREIGN KEY (bubble_id)REFERENCES checkbox_bubble(id);
+
+ALTER TABLE users_checkbox_responses
+ADD CONSTRAINT users_checkbox_responses_user__fk
+FOREIGN KEY (user_ref)REFERENCES user(ref),
+ADD CONSTRAINT users_checkbox_responses_bubble__fk
+FOREIGN KEY (checkboxresponse_id)REFERENCES checkbox_response(id);
+
+ALTER TABLE planning_entry
+ADD CONSTRAINT planning_entry_bubble__fk
+FOREIGN KEY (bubble_id)REFERENCES planning_bubble(id),
+ADD CONSTRAINT planning_entry_user__fk
+FOREIGN KEY (creator_ref)REFERENCES user(ref);
