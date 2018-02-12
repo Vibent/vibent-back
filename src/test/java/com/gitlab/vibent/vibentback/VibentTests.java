f1731f0bc52cfa644df752640026bbed94f85b0e
Conor Ryan <conor.fr.ryan@gmail.com>
2018-02-13 00:31:17 +0100

    Changed default behaviour

--- src/test/java/com/gitlab/vibent/vibentback/VibentTests.java
+++ src/test/java/com/gitlab/vibent/vibentback/VibentTests.java
@@ -9,15 +9,9 @@
 
 package com.gitlab.vibent.vibentback;
 
-import com.gitlab.vibent.vibentback.user.User;
-import com.gitlab.vibent.vibentback.user.UserService;
-import org.junit.Before;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.mockito.Mockito.when;
-
+@ActiveProfiles(value = "test")
 public class VibentTests {
 
 }
