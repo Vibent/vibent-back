4c48b0506386357ccc7421c5400b0982ded18aa8
Conor Ryan <conor.fr.ryan@gmail.com>
2018-02-12 12:54:19 +0100

    Replaced Id with ref where appropriate

--- src/main/java/com/gitlab/vibent/vibentback/user/UserController.java
+++ src/main/java/com/gitlab/vibent/vibentback/user/UserController.java
@@ -1,12 +1,14 @@
 package com.gitlab.vibent.vibentback.user;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -14,8 +16,9 @@ public class UserController {
 
     private final UserService userService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{userId}")
-    User getUser(@PathVariable String userId) {
-        return userService.getUser(userId);
+    @RequestMapping(method = RequestMethod.GET, value = "/{userRef}")
+    User getUser(@PathVariable String userRef) {
+        log.info("Get user with ref : {}", userRef);
+        return userService.getUser(userRef);
     }
 }
