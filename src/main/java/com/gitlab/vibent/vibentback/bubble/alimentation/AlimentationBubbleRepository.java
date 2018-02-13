eb0c761d84361ebb2b0504c3ccd063ebf8d2975a
Conor Ryan <conor.fr.ryan@gmail.com>
2018-02-13 09:58:56 +0100

    Revert "Revert "Merge branch 'feature/inmemeryDbTesting' into 'master'""
    This reverts commit 5dab62fa76f79698fec25ee6c07b82216d43a82b.

--- src/main/java/com/gitlab/vibent/vibentback/bubble/alimentation/AlimentationBubbleRepository.java
+++ src/main/java/com/gitlab/vibent/vibentback/bubble/alimentation/AlimentationBubbleRepository.java
@@ -2,8 +2,6 @@ package com.gitlab.vibent.vibentback.bubble.alimentation;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
-
 
 public interface AlimentationBubbleRepository extends CrudRepository<AlimentationBubble, Long> {
 
