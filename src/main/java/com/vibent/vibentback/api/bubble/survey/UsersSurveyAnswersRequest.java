ea7901a75f20c2d769d7b2e68dd43a07c0ba19fe
JoubertM <theojoubertmoureaud@gmail.com>
2018-03-21 21:48:54 +0100

    Deleted useless userRef in UsersSurveyAnswersRequest

--- src/main/java/com/vibent/vibentback/api/bubble/survey/UsersSurveyAnswersRequest.java
+++ src/main/java/com/vibent/vibentback/api/bubble/survey/UsersSurveyAnswersRequest.java
@@ -1,19 +1,10 @@
 package com.vibent.vibentback.api.bubble.survey;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NonNull;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Data
 public class UsersSurveyAnswersRequest {
-
-    @NonNull
-    private String userRef;
     @NonNull
     private Long surveyAnswerId;
 }
