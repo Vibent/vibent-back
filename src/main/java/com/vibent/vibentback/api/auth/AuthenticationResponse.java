780b31cfdf8b95a54814c07fc49a544533f8adff
Conor Ryan <conor.fr.ryan@gmail.com>
2018-06-09 19:07:21 +0200

    Fixed update descriptin

--- src/main/java/com/vibent/vibentback/api/auth/AuthenticationResponse.java
+++ src/main/java/com/vibent/vibentback/api/auth/AuthenticationResponse.java
@@ -1,20 +1,15 @@
 package com.vibent.vibentback.api.auth;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Value;
-
-import java.time.Instant;
-import java.util.Date;
 
 
 @Data
 public class AuthenticationResponse {
 
     @NonNull
-	private String token;
+    private String token;
 
     @NonNull
-    private long expiresInSeconds;
+    private Long expiresInSeconds;
 }
