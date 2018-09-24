15ef84ada79256a33615a8120c7ab74d84817d5d
Conor Ryan <conor.fr.ryan@gmail.com>
2018-09-24 12:37:37 +0200

    Added way to delete answer of option, in survey and checkbox

--- src/main/java/com/vibent/vibentback/bubble/survey/SurveyController.java
+++ src/main/java/com/vibent/vibentback/bubble/survey/SurveyController.java
@@ -87,4 +87,11 @@ public class SurveyController {
         log.info("Deleting survey answer with id {}", id);
         service.deleteAnswer(id);
     }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @RequestMapping(method = RequestMethod.DELETE, value = "/option/{id}/answer")
+    void deleteAnswerOfOption(@PathVariable Long id) {
+        log.info("Deleting survey answer with option id {}", id);
+        service.deleteAnswerOfOption(id);
+    }
 }
