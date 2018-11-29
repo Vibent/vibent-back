cf7f1e198b961f51ffced6e23d96c28b43538fbf
Conor Ryan <conor.fr.ryan@gmail.com>
2018-11-29 11:46:26 +0000

    Update WebSecurityConfiguration.java

--- src/main/java/com/vibent/vibentback/common/config/WebSecurityConfiguration.java
+++ src/main/java/com/vibent/vibentback/common/config/WebSecurityConfiguration.java
@@ -45,6 +45,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
             "/auth/**"
     };
+    
+    private CorsFilter corsFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
